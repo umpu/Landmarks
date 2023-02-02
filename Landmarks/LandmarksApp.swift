5c366ee06ab6ff3ed13f9d1da0bca1e5e12c5d6a
Andrew Gorbunov <carol.ledgers0h@icloud.com>
2023-02-02 16:30:09 +0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by a on 2/2/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
